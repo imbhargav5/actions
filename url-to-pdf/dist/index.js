e7a624e6a34304f2960e5bf9183ef1037af9f718
imbhargav5 <imbhargav5@gmail.com>
2020-08-21 21:25:12 +0530

    Update

--- url-to-pdf/dist/index.js
+++ url-to-pdf/dist/index.js
@@ -19034,14 +19034,42 @@ module.exports = require("constants");
 
 const core = __webpack_require__(827);
 const fs = __webpack_require__(747).promises;
+const os = __webpack_require__(87);
+const path = __webpack_require__(277);
 
+function getChromePath() {
+    let browserPath;
+  
+    if (os.type() === "Windows_NT") {
+      // Chrome is usually installed as a 32-bit application, on 64-bit systems it will have a different installation path.
+      const programFiles =
+        os.arch() === "x64"
+          ? process.env["PROGRAMFILES(X86)"]
+          : process.env.PROGRAMFILES;
+      browserPath = path.join(
+        programFiles,
+        "Google/Chrome/Application/chrome.exe"
+      );
+    } else if (os.type() === "Linux") {
+      browserPath = "/usr/bin/google-chrome";
+    } else if (os.type() === "Darwin") {
+      browserPath =
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
+    }
+  
+    if (browserPath && browserPath.length > 0) {
+      return path.normalize(browserPath);
+    }
+  
+    throw new TypeError(`Cannot run action. ${os.type} is not supported.`);
+  }
 async function run(){
     try{
         const url = core.getInput('url')
-        const chromePath  = core.getInput('chrome-path');
+        const chromePath  = getChromePath();
         const outputFilePath = core.getInput('output-file-path')
         const createPDF = __webpack_require__(930);
-        const pdf = await createPDF(url, {
+        const pdf = await createPDF(url, {            
             executablePath: chromePath,
             args: ["--no-sandbox", "--disable-setuid-sandbox"]
         });
